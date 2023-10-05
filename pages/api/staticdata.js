c23be5967bd4e2577ce75354773aa54fa8a79290
Joelton Guerreiro <joeltonguerreiro@gmail.com>
2023-10-04 21:14:22 -0300

    set responseLimit to false

--- pages/api/staticdata.js
+++ pages/api/staticdata.js
@@ -9,3 +9,9 @@ export default async function handler(req, res) {
   //Return the content of the data file in json format
   res.status(200).json(fileContents);
 }
+
+export const config = {
+    api: {
+    responseLimit: false,
+    },
+  }
